8313529cde13f5ca5f3c142e35aee411d00b25c0
Dale Wijnand <dale.wijnand@gmail.com>
2016-08-24 17:22:59 +0100

    Add HttpRequestWithToS

--- repl/src/main/scala/ghx/repl/package.scala
+++ repl/src/main/scala/ghx/repl/package.scala
@@ -5,6 +5,8 @@ import scala.concurrent._
 import scala.concurrent.duration._
 import scala.util._
 
+import fr.hmil.roshttp._
+
 object `package` {
   implicit final class FutureWithAwait[A](private val fut: Future[A]) extends AnyVal {
     def await(d: Duration) = Await.result(fut, d)
@@ -18,4 +20,21 @@ object `package` {
         case Some(Success(x)) => s"Future(${show(x)})"
       }
   }
+
+  implicit final class HttpRequestWithToS(private val req: HttpRequest) extends AnyVal {
+    def to_s: String = {
+      import req._
+
+      val headersString = if (headers.isEmpty) "" else s"""
+        |Headers:
+        |  ${headers._map map (t => t._1 + ": " + t._2) mkString "\n  "}
+      """.stripMargin.trim
+
+      s"""
+        |$method $url
+        |$headersString
+      """.stripMargin.trim
+    }
+  }
+
 }
