596d10b60488e6f19c729a7caed818e23aff39ca
Dale Wijnand <dale.wijnand@gmail.com>
2016-09-05 17:31:59 +0100

    wip

--- core/src/main/scala/ghx/Main.scala
+++ core/src/main/scala/ghx/Main.scala
@@ -23,11 +23,14 @@ object GitHub {
 final case class User(name: String) extends AnyVal {
   def repo(name: String) = Repo(this, name)
 
+  def repos = ListRepos(this)
+
   override def toString = name
 }
 
 final case class Repo(owner: User, name: String) {
-  def pulls(number: Int) = {
+  /** https://developer.github.com/v3/pulls/#get-a-single-pull-request */
+  def pullRequest(number: Int) = {
     val req = GitHub.baseRequest withPath s"/repos/$owner/$name/pulls/$number"
     def errMsg0 = s"Failed to get ${req.path}."
     def err1                    = runtimeException(errMsg0)
@@ -43,3 +46,28 @@ final case class Repo(owner: User, name: String) {
       .transform(identity, err1 initCause _)
   }
 }
+
+final case class ListRepos(
+        user: User,
+       _type: ListReposType,
+       _sort: ListReposSort,
+  _direction: ListReposDirection
+) {
+}
+
+sealed class ListReposType(val value: String)
+case object ListReposDefault extends ListReposType("owner")
+case object ListReposAll     extends ListReposType("all")
+case object ListReposOwner   extends ListReposType("owner")
+case object ListReposMember  extends ListReposType("member")
+
+sealed class ListReposSort(val value: String)
+case object ListReposSortDefault    extends ListReposSort("full_name")
+case object ListReposSortByCreated  extends ListReposSort("created")
+case object ListReposSortByUpdated  extends ListReposSort("updated")
+case object ListReposSortByPushed   extends ListReposSort("pushed")
+case object ListReposSortByFullName extends ListReposSort("full_name")
+
+sealed class ListReposDirection(val value: String)
+case object ListRepoosDirectionAsc  extends ListReposDirection("asc")
+case object ListRepoosDirectionDesc extends ListReposDirection("desc")
